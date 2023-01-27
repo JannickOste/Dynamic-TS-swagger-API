add26563a158d826df0731203b18452cb8d4a239
Oste Jannick <jannick@oste.dev>
2023-01-27 17:09:48 +0100

    Removed redundant file

--- src/types/IAPISpecMetadataObject.ts
+++
@@ -1,7 +0,0 @@
-import { OpenAPIV3 } from "express-openapi-validator/dist/framework/types";
-
-export type IAPISpecMetadataObject = {
-    route:string;
-    data:OpenAPIV3.PathItemObject
-    component?:OpenAPIV3.ComponentsObject;
-}
